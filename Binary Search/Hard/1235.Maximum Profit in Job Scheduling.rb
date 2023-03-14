c7c9d3811f9453c0c086a11d7adca7720ab71749
MacbookPro <macbookpro@macbookpros-mbp.lan>
2023-03-14 17:45:58 -0400

    done

--- Binary Search/Hard/1235.Maximum Profit in Job Scheduling.rb
+++ Binary Search/Hard/1235.Maximum Profit in Job Scheduling.rb
@@ -1,117 +1,89 @@
-# Failing approach, too slow. Fails at 15/30 test case.
+# Beats 100% runtime, 25% memory.
 # Approach
-# We need to return the maximum amount that can be made.
-# Go through each possibility and return the the combination of possibilities where we create the most profit.
-# We can try to coming up with a sorted array by somehow combining values from start time and end time.
-startTime = [1,2,3,4,6],
-endTime = [3,5,10,6,9]
-profit = [20,20,100,70,60]
-
-
-combinedTime = [1,2.9,3, 9,9]
-combinedProfit = [20,100]
-
+# 1. Create array containing start_time, end_time and profit into one array, lets call it jobs_data.
+# 2. Sort jobs_data by start_time and reverse jobs_data. Important to reverse the array as we need to memo all the calculations and determine the highest profit so we can reuse it later.
+# 3. Sort start_time array.
+# 4. Transverse the jobs_data array and when we need to find the jobs that are still available based on start time, get the highest profit job and add it to the current job profit then memo the total profit using the index of the job as the memo key. Make sure to store the maximum profit if it exceeds the previous maximum profit value.
+# 5. Return maximum profit when we are all down tranversing the jobs_data array.
 
 # @param {Integer[]} start_time
 # @param {Integer[]} end_time
 # @param {Integer[]} profit
 # @return {Integer}
+# Time Complexity: O(n log n) due to using quicksort to sort arrays.
+# Space Complexity: O(n) creating a few arrays and a hash.
 def job_scheduling(start_time, end_time, profit)
 
+    # Collect all job data from start_time, end_time and profit into one array.
     index = 0
-    max_index = start_time.size - 1
-    max_profit = 0
-
-    index = 0
-    job_data = []
-    last_job_index = start_time.size
-    while index < last_job_index
-        job_data << [start_time[index], end_time[index], profit[index]]
+    jobs_size = start_time.size
+    jobs_data = []
+    while index < jobs_size
+        jobs_data << [start_time[index], end_time[index], profit[index]]
         index = index + 1
     end
 
-    job_data.sort!{|a, b| a[0] <=> b[0]}
-    start_time = job_data.collect{|a| a[0]}
-    max_profit_calc = MaxProfit.new(start_time, job_data)
+    # Sort job data by start time and then reverse the array.
+    # Time Complexity: O(n log n)
+    # Space Complexity: O(log n)
+    jobs_data.sort_by!{|job| job[0]}.reverse!
+
+    # Sort start time.
+    # Time Complexity: O(n log n)
+    # Space Complexity: O(log n)
+    start_time = start_time.sort!
+
+    # Traverse the job data array going through each job in reverse order.
+    # Calculate job_sorted_by_start_time_index(index if we sorted by start time, remember the array is currently reversed).
+    @max_profit = 0
+    @memo = {}
     index = 0
-    while index < max_index
-         max_profit_calc.maximum_profit_from_starting_time job_data[index]
-         index = index + 1
+    job_sorted_by_start_time_index = jobs_size - 1
+    while index < jobs_size
+        # Set job data based on index.
+        job_data = jobs_data[index]
+        # Update end time
+
+        # Set job time.
+        job_end_time = job_data[1]
+
+        # Profit for job
+        job_profit = job_data[2]
+
+        # Combine current job profit with the accumulated value of
+        # jobs available(based on end time) after completing the current job.
+        job_profit = job_profit + next_job_profit(job_end_time, start_time, @memo)
+
+        # Set @max_profit if current job profit is larger than the current @max_profit.
+        if job_profit > @max_profit
+            @max_profit = job_profit
+        end
+
+        # Memo the job profit.
+        # We set max profit here despite there being multiple possibilities
+        # for available jobs.
+        # This is because only the maximum profit is the only profit we are interest in and we save on runtime if we do not check all possibilities.
+        @memo[job_sorted_by_start_time_index] = @max_profit
+
+        # Update index and job_sorted_by_start_time_index
+        index = index + 1
+        job_sorted_by_start_time_index = job_sorted_by_start_time_index - 1
     end
 
-    max_profit_calc.max_profit
+    @max_profit
 end
 
-class MaxProfit
-    attr_accessor :max_profit
-    def initialize(start_time, job_data)
-        @start_time = (start_time)
-        @job_data = job_data
-        @last_job_index = @start_time.size - 1
-        @max_profit = 0
+# Returns the profit from the next job proceeding end_time(if available).
+# Time Complexity: O(log n)
+# Space Complexity: O(log n)
+def next_job_profit end_time, start_time, memo
+    # Use binary search to find the next job start time index.
+    next_job_index = start_time.bsearch_index{|t| t >= end_time }
+
+    # If we find a next job index, return the profit contained within our memo.
+    if next_job_index
+        return memo[next_job_index]
+    else
+        return 0
     end
-
-    # We are gathering a list of job indexes recursively based on start and end times
-    # Then we will do the profit calculation on each combination and return the profit.
-    def maximum_profit_from_starting_time job_data
-        current_profit = (gather_job_possibilities_based_on_first_job job_data)
-        if current_profit > @max_profit
-            @max_profit = current_profit
-        end
-    end
-
-    def gather_job_possibilities_based_on_first_job job_data, end_time = 0, current_val = 0
-        possibilities = []
-        # Get the first job profit
-        # Update end time
-        start_time = job_data[0]
-
-        end_time = job_data[1] - 0.1
-
-        current_val = current_val + job_data[2]
-
-
-        # Then get remaining available jobs after completing the job.
-        next_jobs_start_index = available_jobs_start_index(end_time)
-        jobs_indexes = []
-
-        if next_jobs_start_index
-            jobs_indexes = (next_jobs_start_index..@last_job_index)
-        end
-
-        #return jobs_indexes
-        current_possibility = current_val
-        current_max_profit = current_val
-        if jobs_indexes.size > 0
-
-            jobs_indexes.each do |job_index|
-                 job_index_results = (gather_job_possibilities_based_on_first_job @job_data[job_index], end_time, current_val)
-
-                if current_max_profit < job_index_results
-                    current_max_profit = job_index_results
-                end
-
-            end
-            return current_max_profit
-        else
-            return current_possibility
-        end
-    end
-
-
-    # Returns a list of start time indexes that are greater than end_time
-    def available_jobs_start_index end_time
-        # Return the index of the first job where end_time > start_time
-
-        #next_job_index = @memod_start_time[index].bsearch_index{|t| t > end_time }
-        next_job_index = @start_time.bsearch_index{|t| t > end_time }
-
-        if next_job_index
-            return next_job_index
-        else
-            return nil
-        end
-    end
-
-
 end
